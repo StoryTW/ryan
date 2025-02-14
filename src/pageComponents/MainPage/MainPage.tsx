8bb4bc902ddc819c42b4a9cdb7fa8869ff96a45e
parienko_sergej <parienko_sergej@enot.work>
2025-02-15 01:55:28 +0300

    fix

--- src/pageComponents/MainPage/MainPage.tsx
+++ src/pageComponents/MainPage/MainPage.tsx
@@ -1,3 +1,4 @@
+import HowToBuySection from '@/sections/HowToBuySection/HowToBuySection';
 import MeetSection from '@/sections/MeetSection/MeetSection';
 import dynamic from 'next/dynamic';
 import React from 'react';
